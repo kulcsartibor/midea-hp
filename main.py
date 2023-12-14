963a21ae479eacab9f7e81bbca86bfca4f0a093d
Tibor Kulcsar <tibor.kulcsar@mavoco.com>
2023-12-14 19:04:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+"""The Midea Heat pump Modbus integration """
+
+
+from __future__ import annotations
+
+import asyncio
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
